7838f1369a755be7e477b9830a4b7699aeb7d3d2
BuildTools <unconfigured@null.spigotmc.org>
2021-03-07 15:06:23 -0500

    card class complete

---
+++ playFlip.cpp
@@ -0,0 +1,10 @@
+/*
+ * By: Sergey Petrushkevich & Luke Ackerman
+ * Project 2, Part B
+ * Function definition for playFlip global function.
+ */
+
+#include <ctime>
+#include <algorithm>
+#include "playFlip.h"
+
---
+++ playFlip.h
@@ -0,0 +1,19 @@
+/*
+ * By: Sergey Petrushkevich & Luke Ackerman
+ * Project 2, Part B
+ * Contains playFlip global function.
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include "card.h"
+
+#ifndef PLAYFLIP_H
+#define PLAYFLIP_H
+
+using namespace std;
+
+
+#endif //PLAYFLIP_H
